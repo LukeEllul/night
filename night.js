80f2b3affdfd254d3ca0dbfb01443fbdf65e6533
LukeEllul <ellul.luke@gmail.com>
2017-09-11 00:31:36 +0200

    added mechanism to check if nightmare has finished i.e to check if promise has fulfilled or rejected

--- night.js
+++ night.js
@@ -2,36 +2,54 @@ const Nightmare = require('nightmare');
 require('trina');
 const { List } = require('immutable');
 const R = require('ramda');
-const get = require('get-parameter-names');
+const  getParameters  =  require('get-parameter-names');
 
 const NightmareOptions = {
     show: true,
     waitTimeout: 30000
 }
 
+const google = 'https://www.google.com';
+
 const nightmare = Nightmare(NightmareOptions);
 
 const night = (...commands) => {
     const nextFn = (...args) => fn => {
         const isFunction = typeof fn === 'function';
+        let flag = [false];
+        let resultofFlagFn = null;
 
-        const thisFn = nightmare => {
-            let flag = false;
-            if(nightmare === 'has nightmare finished') return flag;
-            const thisNightmare = nightmare.constructor === Array ?
-                nightmare[0] : nightmare;
-            const resolutionFn = resolution => v => {
-                flag = true;
-                const result = isFunction ? fn(v,nightmare[1]) : v;
-                const rFn = r => resolution([thisNightmare, r]);
-                return result && result.then ? 
-                    result.then(rFn, rFn) : resolution([thisNightmare, result]);
+        const thisFn = flagFn => {
+            const mainFn = nightmare => {
+                const thisNightmare = nightmare.constructor === Array ?
+                    nightmare[0] : nightmare;
+                const resolutionFn = resolution => v => {
+                    flag[0] = true;
+                    const result = isFunction ? fn(v, nightmare[1], resultofFlagFn) : v;
+                    const rFn = r => resolution([thisNightmare, r]);
+                    return result && result.then ?
+                        result.then(rFn, rFn) : resolution([thisNightmare, result]);
+                }
+
+                return new Promise((res, rej) =>
+                    thisNightmare[commands[0]](...args)
+                        .then(resolutionFn(res), resolutionFn(rej)));
             }
-            return new Promise((res, rej) => 
-                thisNightmare[commands[0]](...args)
-                .then(resolutionFn(res),resolutionFn(rej)));
+
+            return typeof flagFn === 'function' ? 
+                (resultofFlagFn = flagFn(flag), mainFn) : mainFn(flagFn);
         }
         return isFunction ? thisFn : thisFn(fn);
     }
     return commands.length === 1 ? nextFn : nextFn(...commands.slice(1));
 }
+
+const evaluate = fn => night('evaluate', fn, ...getParameters(fn));
+
+trina(
+    night('goto', google)((v, j, f) => (console.log(f), f))
+        (f => (console.log(f), f)), AND,
+    evaluate(() => document.querySelector('div.jsb center input').value)
+        ((v, j) => (console.log(v), console.log('now flag is ' + j)))
+)
+    (nightmare);
